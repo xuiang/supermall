4ed8a846feb6cd340ecc926de470756da855b915
xuiang <52398067+xuiang@users.noreply.github.com>
2021-05-20 21:57:45 +0800

    recommend补充

--- src/views/home/childrenComponents/homeRecommend.vue
+++ src/views/home/childrenComponents/homeRecommend.vue
@@ -24,11 +24,11 @@ export default {
 .recommend{
   display:flex;
   width:100%;
-  height:100px;
+  height:120px;
   justify-content:space-evenly;
   text-align:center;
   font-size:12px;
-
+  border-bottom: 8px solid #ddd;
 }
 .recommend > .recommend-item > a{
   color:rgb(36,36,36);
@@ -36,7 +36,7 @@ export default {
 .recommend > .recommend-item img{
   display:block;
   width:80%;
-  margin:5px auto;
+  margin:7px auto;
 }
 
 </style>
