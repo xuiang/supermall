705c63f5e1b6a21a0ce70dfbf96c7476ac9828e7
xuiang <52398067+xuiang@users.noreply.github.com>
2021-05-21 21:23:22 +0800

    做好了每周精选

---
+++ src/views/home/childrenComponents/weekTop.vue
@@ -0,0 +1,21 @@
+vue-<template lang="">
+  <div class='weekTop'>
+    <a href='https://act.mogujie.com/zzlx67'>
+      <img src='~assets/img/home/recommend_bg.jpg' alt='weekTop'>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    weekTop: {
+
+    }
+  }
+}
+</script>
+<style>
+  .weekTop img{
+    width:100%;
+  }
+</style>
